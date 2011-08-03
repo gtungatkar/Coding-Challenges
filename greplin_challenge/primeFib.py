ea586f7768d3c157c5c30febe121904f7b2a72fc
gtungatkar <gauravstt@gmail.com>
2011-08-03 00:22:20 -0400

    greplin challenge 2-bug in prime number fixed

--- greplin_challenge/primeFib.py
+++ greplin_challenge/primeFib.py
@@ -3,14 +3,14 @@ import sys;
 
 def isprime_opt(n):
         #make it float
-        n = n * 0.1;
+        n = n * 1.0;
         if(n == 2 or n == 3):
                 return True;
         if(n <= 0):
                 return False;
         if(n % 2 == 0):
                 return False;
-        for i in xrange(1, int((n**0.5+1)/6.0+1)):
+        for i in range(1, int((n**0.5+1)/6.0+1)):
                 if n % (6*i-1) == 0:
                         return False;
                 if n % (6*i+1) == 0:
@@ -19,15 +19,15 @@ def isprime_opt(n):
 
 def isprime(n):
         #make it float
-        n = n * 0.1;
         if(n == 2 or n == 3):
                 return True;
         if(n <= 0):
                 return False;
+        n = n * 1.0;
         if(n % 2 == 0):
                 return False;
-        for i in xrange(3, int(n**0.5)+1,2):
-                if n % i == 0:
+        for i in range(3, int(n**0.5)+1, 2):
+                if int(n) % i == 0:
                         return False;
         return True;          
 
@@ -45,5 +45,12 @@ def primeFib(minNum):
 			if isprime(f):
 				return f;
 	return 0;	
-print primeFib(217000);
-print isprime(317811);
+
+def primeFact(n):
+        i=1
+        while~-n:
+                i+=1
+               # print n
+                while n%i<1:print i;n/=i
+#print primeFib(217000);
+print primeFact(primeFib(217000)+1)
